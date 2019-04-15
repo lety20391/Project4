64f4b49a114fe2b53fa014b56663353076c28d37
lety20391 <congdatrhm@gmail.com>
2019-04-15 09:59:29 +0700

    fix import css error
    fix import css error

--- Front_End/Angular-Front-End/src/app/app.component.ts
+++ Front_End/Angular-Front-End/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css',
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-Front-End';
+
+  ngOnInit(){
+    this.loadScript('./assets/js/vendor/modernizr.min.js');
+  }
+
+  public loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
 }
