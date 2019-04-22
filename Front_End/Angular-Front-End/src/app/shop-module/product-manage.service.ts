dc842fa879a19e47373ab88dd99c4d3e0f2ed763
lety20391 <congdatrhm@gmail.com>
2019-04-22 08:00:36 +0700

    add UrlResource API
    manage all API resource here

--- Front_End/Angular-Front-End/src/app/shop-module/product-manage.service.ts
+++ Front_End/Angular-Front-End/src/app/shop-module/product-manage.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { productEntity } from '../productEntity/productEntity';
 import {Observable, of} from 'rxjs';
+import { listUrlAPI } from '../listUrlAPI';
+import { UrlAPIEntity } from '../UrlAPIEntity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductManageService {
 
+  urlAPI: UrlAPIEntity;
   constructor(
     private http: HttpClient
   ) { }
@@ -15,6 +18,8 @@ export class ProductManageService {
   getProductList(): Observable<productEntity[]> {
     console.log("------Get API Product Service ------");
     //console.log(this.http.get<productEntity[]>("http://localhost:34828/1Hero-web/rest/product/"));
-    return this.http.get<productEntity[]>("http://localhost:8080/1Hero-web/rest/product");
+
+    this.urlAPI = listUrlAPI.find(url => url.name === 'productResource');
+    return this.http.get<productEntity[]>(this.urlAPI.path);
   }
 }
