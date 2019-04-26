e2c4125736db5c3d87ea72e9a2886e67c9f9c956
lety20391 <congdatrhm@gmail.com>
2019-04-26 16:21:29 +0700

    test Mobile Serive -> OK
    Mobile will post to eSMS and user will receive authorization code via SMS, and Login will check code

--- duy/ServicePRJ/ServicePRJ-web/src/main/java/com/sam/web/manageBean/LoginManagedBean.java
+++ duy/ServicePRJ/ServicePRJ-web/src/main/java/com/sam/web/manageBean/LoginManagedBean.java
@@ -8,6 +8,7 @@ package com.sam.web.manageBean;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
@@ -16,34 +17,44 @@ import javax.inject.Named;
  * @author Dat Le
  */
 @ManagedBean(name="loginManagedBean")
-@ViewScoped
+@SessionScoped
 public class LoginManagedBean implements Serializable{
     
-    private String name = "";
+    private String phone = "";
     private String pass = "";
+    private String code = "";
+    private MobileService mobileService = new MobileService();
     
     @PostConstruct
     public void init(){
         //this.name = "test";
         //this.pass= "test";
-        System.out.println("Init Current User: " + name);
+        System.out.println("Init Current User: " + phone);
     }
     
     public String checkLogin(){
         System.out.println("-----LoginMB: checkLogin()-------");
-        System.out.println("Current User: " + name + "-" + pass);
-        if ( "admin".equals(name) && "abc123".equals(pass))
+        System.out.println("Current User: " + phone + "-" + pass);
+        if ( this.code.equals(pass))
             return "success";
         return "Login";
     }
+    
+    public void getCode(){
+        this.code = "abcdef";
+        System.out.println("----ManagedBean: getCode(): " + this.code);
+        mobileService.getMobileCode(this.code);
+        //return "Login";
+    }
+    
 
-    public String getName() {
-        return name;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }    
 
     public String getPass() {
         return pass;
---
+++ duy/ServicePRJ/ServicePRJ-web/src/main/java/com/sam/web/manageBean/MobileService.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sam.web.manageBean;
+
+import java.io.IOException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+/**
+ *
+ * @author Dat Le
+ */
+public class MobileService {
+    public String getMobileCode(String hashCode){
+        String url = "http://rest.esms.vn/MainService.svc/json/SendMultipleMessage_V4_post_json/";
+        //Noi dung thay doi trong content
+        String body = " {\n" +
+"	\"ApiKey\":\"2EFF2FDBB7A6AD84DFC2488C6DF704\",\n" +
+"	\"Brandname\":\"Verify\",\n" +
+"	\"Content\":\"Ma xac nhan dang nhap cua ban la: " + hashCode +"\",\n" +
+"	\"Phone\":\"0394641271\",\n" +
+"	\"SecretKey\":\"644034136AA870DCCD960255F76543\",\n" +
+"	\"SmsType\":\"2\"\n" +
+"} ";
+        try ( CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpPost request = new HttpPost(url);
+            StringEntity params = new StringEntity(body);
+            request.addHeader("content-type", "application/json");
+            request.setEntity(params);
+            HttpResponse result = httpClient.execute(request);
+            String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+
+            com.google.gson.Gson gson = new com.google.gson.Gson();
+            Response respuesta = gson.fromJson(json, Response.class);
+
+            System.out.println(respuesta.getExample());
+            System.out.println(respuesta.getFr());
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        String code = "";
+        return code;
+    }
+    
+    
+    public class Response{
+        private String example;
+        private String fr;
+
+        public String getExample() {
+            return example;
+        }
+        public void setExample(String example) {
+            this.example = example;
+        }
+        public String getFr() {
+            return fr;
+        }
+        public void setFr(String fr) {
+            this.fr = fr;
+        }
+    }
+}
