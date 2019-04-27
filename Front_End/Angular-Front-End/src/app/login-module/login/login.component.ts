72f83144b3df4b19666e6129fd0f3642fc827842
lety20391 <congdatrhm@gmail.com>
2019-04-27 18:32:43 +0700

    Update login.component.ts
    update httpOptions

--- Front_End/Angular-Front-End/src/app/login-module/login/login.component.ts
+++ Front_End/Angular-Front-End/src/app/login-module/login/login.component.ts
@@ -8,8 +8,8 @@ import { HttpHeaders } from '@angular/common/http';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type':  'application/json',
-    'Authorization': 'my-auth-token'
+    'Content-Type':  'application/json'
+    // 'Authorization': 'my-auth-token'
   })
 };
 
