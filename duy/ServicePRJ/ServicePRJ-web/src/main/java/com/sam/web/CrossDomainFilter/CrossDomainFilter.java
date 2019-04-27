9bee6a5f6a615ad9658f604887d4f4bb52705376
lety20391 <congdatrhm@gmail.com>
2019-04-27 17:54:13 +0700

    add Cross Orgin Jersey
    add Cross Orgin Jersey
    ---Error no Runtime Provider

---
+++ duy/ServicePRJ/ServicePRJ-web/src/main/java/com/sam/web/CrossDomainFilter/CrossDomainFilter.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sam.web.CrossDomainFilter;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+import java.io.IOException;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author Dat Le
+ */
+@Provider
+public class CrossDomainFilter implements ContainerResponseFilter {
+
+    @Override
+    public ContainerResponse filter(ContainerRequest creq, ContainerResponse cres) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("----CrossDomainFilter: init----");
+        cres.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+        cres.getHttpHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        cres.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
+        cres.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        cres.getHttpHeaders().add("Access-Control-Max-Age", "1209600");
+        System.out.println("----CrossDomainFilter: finish----");
+        return cres;
+    }
+
+    
+    
+}
--- duy/ServicePRJ/ServicePRJ-web/src/main/java/com/sam/web/rest/ApplicationConfig.java
+++ duy/ServicePRJ/ServicePRJ-web/src/main/java/com/sam/web/rest/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.sam.web.CrossDomainFilter.CrossDomainFilter.class);
         resources.add(com.sam.web.rest.AuthResource.class);
         resources.add(com.sam.web.rest.ManageBookingDetail.class);
         resources.add(com.sam.web.rest.ManageBookingMaster.class);
