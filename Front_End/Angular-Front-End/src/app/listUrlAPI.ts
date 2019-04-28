449a9c9de18ec9cccc001a5b2bb61f011b419b66
lety20391 <congdatrhm@gmail.com>
2019-04-28 17:00:47 +0700

    test Send Token --error
    dont have any Authorization token

--- Front_End/Angular-Front-End/src/app/listUrlAPI.ts
+++ Front_End/Angular-Front-End/src/app/listUrlAPI.ts
@@ -4,7 +4,7 @@ import { UrlAPIEntity } from './UrlAPIEntity';
 const localhost = 'http://localhost:34828';
 
 export const listUrlAPI: UrlAPIEntity[] = [
-  {name:'productResource', path: localhost + '/ServicePRJ-web/rest/product'},
+  {name:'productResource', path: localhost + '/ServicePRJ-web/rest/BookingDetail'},
   {name:'loginResource', path: localhost + '/ServicePRJ-web/rest/tokens'},
   {name: 'YourResource', path: localhost + 'YourURL like example above'}
 ];
