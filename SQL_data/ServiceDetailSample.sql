823243395de8a347df819d7a21fa5d70a7e1b977
Duy Tran <duytran17796@gmail.com>
2019-04-30 07:44:57 +0700

    add sample data service

---
+++ SQL_data/ServiceDetailSample.sql
@@ -0,0 +1,23 @@
+use Prj4
+go
+
+Insert into dbo.tbService(SerName, SerDes, SerShortDes, SerImage,[Status]) values
+('Pet Grooming','Grooming is an important part of your pets care. All pets benefit from regular grooming, whether it is a short haired breed or one with a long or fluffy coat, regular grooming helps to ensure your pet is healthy and comfortable.
+
+Keeping your pet’s coat clean by removing dirt and dead hair helps encourage new hair growth and reduces the amount of hair deposited on household furniture. Grooming regularly helps lower the chance of various health problems, such as parasite infestation, thrush, scratches, and other skin problems. Grooming also helps to stimulate new coat growth and prevents the formation of knots and tangles which may lead to skin irritation. Habitual grooming allows monitoring of the pet’s health by checking for cuts, heat, swelling, lameness, or changes in temperament, all of which could be indicative of illness forging of a closer bond between pet and owner. ','Making dogs & cats look great is our passion!','https://i.ibb.co/nw9vBDT/600x600barber.jpg',1),
+('Vaccination Supply','Pet vaccines exist to prevent your pet from falling ill. They provide immunity from a range of infectious diseases that can affect both humans and animals.
+
+In order to be effective, vaccines need to contain an agent similar to the microorganism that causes the disease. Once injected, this agent stimulates the body’s immune system, allowing the body to recognise it as a threat. The immune system will then attack the foreign agent and remember it so that in the future, should the body encounter the same disease, the immune system will be prepared to fight it off.
+
+It is essential to know that vaccines work better in a healthy and relaxed animal. It usually takes around seven days for the body to respond and develop immunity. Therefore, giving a vaccine to your pet while it is already sick will be less effective. Vaccines do not work as a cure for diseases, but rather as a prevention.','Keeping your pet’s vaccinations up-to-date is crucial for a healthy lifestyle and proper pet development.','https://i.ibb.co/jg1K6WK/600x600vacine.jpg',1),
+('Pet Hotel','Stay Pet Hotel  offers a unique combination of services and features, such as a choice between kennel-free boarding and comfortable private suites, daycare 7 days a week, and 24 hour supervision… meaning that your pups are never left unattended!
+
+Stay is located on NE Columbia Blvd, less than 10 minutes from PDX, and we have staff on site 24 hours a day, 365 days a year. The dogs in our care are never left unattended.
+
+Why STAY is THE place for your dog while you’re away:
+
+A choice between kennel-free overnight boarding, or comfortable private suites.
+Huge outdoor space with artificial grass and play structures.
+Swimming pools and custom made sprinklers in the summer.
+Lots of heated and air conditioned indoor play space, which opens up into our outdoor area! They have the choice to play inside or outside.
+24 hour supervision!','Posh Pet Hotel provides our boarding pet guests with the highest quality of comfort and care','https://i.ibb.co/jrQBhyk/600x600hotel.jpg',1)
