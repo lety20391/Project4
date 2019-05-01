b8d16be29ad3b5c55b0e284cf9abb706d9bdef51
lety20391 <congdatrhm@gmail.com>
2019-05-01 12:05:03 +0700

    fix 2 dropdown list
    fix 2 dropdown list

--- Front_End/Angular-Front-End/src/assets/js/search.js
+++ Front_End/Angular-Front-End/src/assets/js/search.js
@@ -26,3 +26,23 @@ var slider = document.getElementById("myRange");
 			}
 		  }
 		}
+
+		function myFunction2() {
+		  document.getElementById("myDropdown2").classList.toggle("show");
+		}
+
+		function filterFunction2() {
+		  var input, filter, ul, li, a, i;
+		  input = document.getElementById("myInput2");
+		  filter = input.value.toUpperCase();
+		  div = document.getElementById("myDropdown2");
+		  a = div.getElementsByTagName("a");
+		  for (i = 0; i < a.length; i++) {
+			txtValue = a[i].textContent || a[i].innerText;
+			if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			  a[i].style.display = "";
+			} else {
+			  a[i].style.display = "none";
+			}
+		  }
+		}
