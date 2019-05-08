e421931320e5308b094472360afe89fd54e66eb7
elisamuoi <tranthituyetduyen@yahoo.com>
2019-05-08 19:46:09 +0700

    up

--- Front_End/Angular-Front-End/src/app/listUrlAPI.ts
+++ Front_End/Angular-Front-End/src/app/listUrlAPI.ts
@@ -4,7 +4,7 @@ import { UrlAPIEntity } from './UrlAPIEntity';
 const localhost = 'http://localhost:8080';
 
 export const listUrlAPI: UrlAPIEntity[] = [
-  {name:'productResource', path: localhost + '/1Hero-web/rest/product'},
+  {name:'productResource', path: localhost + '/ServicePRJ-web/rest/BookingDetail'},
   {name:'loginResource', path: localhost + '/ServicePRJ-web/rest/tokens'},
   {name: 'YourResource', path: localhost + 'YourURL like example above'},
   {name: 'serviceResource', path: localhost + '/ServicePRJ-web/rest/Service/'},
