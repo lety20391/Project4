f0a897940704bdedca3b45be92983367c1de9d34
lety20391 <congdatrhm@gmail.com>
2019-05-14 13:44:33 +0700

    Update app-routing.module.ts

--- Front_End/Angular-Front-End/src/app/app-routing.module.ts
+++ Front_End/Angular-Front-End/src/app/app-routing.module.ts
@@ -77,7 +77,7 @@ const routes: Routes = [
   imports: [
 
     CommonModule,
-    RouterModule.forRoot(routes, { enableTracing: true })
+    RouterModule.forRoot(routes, { enableTracing: true, useHash: true })
   ],
   exports: [RouterModule]
 })
