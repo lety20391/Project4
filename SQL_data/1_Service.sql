f536d4e8227772cc660c1792fc7cf217e7bc9525
lety20391 <congdatrhm@gmail.com>
2019-05-19 08:07:48 +0700

    Update 1_Service.sql

--- SQL_data/1_Service.sql
+++ SQL_data/1_Service.sql
@@ -1,17 +1,17 @@
 ﻿
 Use Prj4
-
+GO
 Insert into dbo.tbUser([userDOB], [userMail], [userName], [userStatus], [userTel]) Values 
-(1996-10-20,'lecongdat@gmail.com','DatLe',1,'0394641271'),
-(1995-10-20,'ElisaMuoi@gmail.com','Elisa',1,'dat'),
-(1994-10-20,'duytantran@gmail.com','Duy',1,'0708886925'),
-(1993-10-20,'vohuynhnhu@gmail.com','Nichai',1,'0909090909'),
-(1993-10-20,'leminhtruyen@gmail.com','Trennle',1,'0909090908'),
-(1993-10-20,'andrewgoodall@gmail.com','Andy',1,'0909090909'),
-(1993-10-20,'tranphanynhi@gmail.com','SuuNhi',1,'0909090908')
+('1996-10-20','lecongdat@gmail.com','DatLe',1,'0394641271'),
+('1995-10-20','ElisaMuoi@gmail.com','Elisa',1,'dat'),
+('1994-10-20','duytantran@gmail.com','Duy',1,'0708886925'),
+('1993-10-20','vohuynhnhu@gmail.com','Nichai',1,'0909090909'),
+('1993-10-20','leminhtruyen@gmail.com','Trennle',1,'0909090908'),
+('1993-10-20','andrewgoodall@gmail.com','Andy',1,'0909090907'),
+('1993-10-20','tranphanynhi@gmail.com','SuuNhi',1,'0909090906')
 
 
-
+GO
 Insert into dbo.tbService(SerName, SerDes, SerShortDes,[Status]) values
 ('Pet Grooming','Grooming is an important part of your pets care. All pets benefit from regular grooming, whether it is a short haired breed or one with a long or fluffy coat, regular grooming helps to ensure your pet is healthy and comfortable.
 
@@ -39,14 +39,26 @@ Lots of heated and air conditioned indoor play space, which opens up into our ou
 ('Surgery','At Pet Paradise Doctors our veterinarians carry out surgery in the security of our dedicated surgical suite, using specialist equipment and in a sterile environment. Specially trained veterinary surgeons monitor the anaesthesia with modern equipment throughout the procedure and our surgeons employ the most modern surgical technques.','Dedicated. Compassionate. Skilled.',1)
 
 
+GO
+Insert into dbo.tbBookingMaster ([creDate] , [bmStatus] , [userID]) values
+('2019-04-21',1,1),
+('2019-04-22',1,2),
+('2019-04-23',1,3),
+('2019-04-24',1,4),
+('2019-04-25',1,5)
 
-Insert into dbo.tbBookingMaster values
-(2019-04-21,1,1),
-(2019-04-22,1,2),
-(2019-04-23,1,3),
-(2019-04-24,1,4),
-(2019-04-25,1,5)
+GO
+Insert into dbo.tbPet ( [petDating] ,[petBreed], [petDOB], [petImage], [petName], [petPrice], [petStatus], [userID], [petStory]) values
+(1,'Cardigan Welsh Corgi', '2017-1-1', 'null', 'MinMin', 120, 1, 1, 'The Cardigan Welsh Corgi is the older of the two Corgi dog breeds, with dogs of this type believed to have existed in Wales for more than 3,000 years. The Cardigan is distinguished by his long tail — like the sleeves of a cardigan sweater — and was used to drive cattle to market. Known as the yard-long dog, the Cardigan is sensible and affectionate. He loves to spend time with his family and is an active, fun-loving playmate for school-age children. The Cardigan’s medium-length coat comes in many colors and patterns, including red, brindle, blue merle and black, usually with white markings.'),
+(0,'Bolognese', '2017-1-1', 'null', 'Milk', 20, 1, 1, 'the Bolognese dog breed loves to be at his family’s side. However, he also loves getting his way and can be quite crafty about it, so be careful — you could find yourself being manipulated by a 10-pound furball.'),
+(1,'Boxer', '2017-1-1', 'null', 'Mi', 15, 1, 2, 'Boxers were originally bred to be medium-size guard dogs. Today, although they are a part of the AKC’s Working Group, they mostly find homes as loving family companions.'),
+(1,'Chinook', '2017-1-1', 'null', 'Kiki', 19, 1, 3, 'Created in the White Mountains of New Hampshire, the Chinook dog breed made his name on Admiral Byrd’s first Antarctic expedition in 1928. These days he’s a multipurpose dog who’s happy hiking, competing in agility and other dog sports, pulling a sled or other conveyance, and playing with the kids.'),
+(1,'Japanese Chin', '2017-1-1', 'null', 'Kuku', 19, 1, 2, 'The Japanese Chin dog breed hails from Asia, where he has been prized as a companion for more than a thousand years. He was a popular member of Chinese and Japanese imperial courts, and it was in Japan that his distinctive look was developed. This breed is elegant and dainty, mild-mannered and playful.'),
+(1,'Pembroke Welsh Corgi', '2017-1-1', 'null', 'Lulu', 19, 1, 2, 'Originally bred to herd cattle, sheep, and horses, the Pembroke Welsh Corgi is an active and intelligent dog breed. Easy to train and eager to learn, Pembrokes are great with children and other pets, and you can find them in four different coat colors and markings.'),
+(1,'Poodle', '2017-1-1', 'null', 'Muc', 19, 1, 3, 'Elegant. Proud. Clever. Poodles are impressive dogs, as the many best-in-show winners from this dog breed can attest. Behind the blue ribbons, impressive hairdos, and regal attitude, you’ll find an affectionate family dog with an ancient history and many talents.'),
+(1,'Pomeranian', '2017-1-1', 'null', 'Nini', 19, 1, 3, 'Descended from large sled dog breeds, the now-tiny Pomeranian has a long and interesting history. The foxy-faced dog, nicknamed “the little dog who thinks he can,” is compact, active, and capable of competing in agility and obedience or simply being a family friend.')
 
+GO
 Insert into dbo.tbBookingDetail ([bookingDate], [bdstatus],[bookingID], [serID],[petID]) values
 ('2019-4-15',1,1,1,1),
 ('2019-4-16',1,2,2,2),
@@ -58,22 +70,14 @@ Insert into dbo.tbBookingDetail ([bookingDate], [bdstatus],[bookingID], [serID],
 ('2019-4-22',1,3,3,5),
 ('2019-4-23',1,5,1,4)
 
-Insert into dbo.tbPet ( [petDating] ,[petBreed], [petDOB], [petImage], [petName], [petPrice], [petStatus], [userID], [petStory]) values
-(1,'Cardigan Welsh Corgi', '2017-1-1', 'null', 'MinMin', 120, 1, 1, 'The Cardigan Welsh Corgi is the older of the two Corgi dog breeds, with dogs of this type believed to have existed in Wales for more than 3,000 years. The Cardigan is distinguished by his long tail — like the sleeves of a cardigan sweater — and was used to drive cattle to market. Known as the yard-long dog, the Cardigan is sensible and affectionate. He loves to spend time with his family and is an active, fun-loving playmate for school-age children. The Cardigan’s medium-length coat comes in many colors and patterns, including red, brindle, blue merle and black, usually with white markings.'),
-(0,'Bolognese', '2017-1-1', 'null', 'Milk', 20, 1, 1, 'the Bolognese dog breed loves to be at his family’s side. However, he also loves getting his way and can be quite crafty about it, so be careful — you could find yourself being manipulated by a 10-pound furball.'),
-(1,'Boxer', '2017-1-1', 'null', 'Mi', 15, 1, 2, 'Boxers were originally bred to be medium-size guard dogs. Today, although they are a part of the AKC’s Working Group, they mostly find homes as loving family companions.'),
-(1,'Chinook', '2017-1-1', 'null', 'Kiki', 19, 1, 3, 'Created in the White Mountains of New Hampshire, the Chinook dog breed made his name on Admiral Byrd’s first Antarctic expedition in 1928. These days he’s a multipurpose dog who’s happy hiking, competing in agility and other dog sports, pulling a sled or other conveyance, and playing with the kids.'),
-(1,'Japanese Chin', '2017-1-1', 'null', 'Kuku', 19, 1, 2, 'The Japanese Chin dog breed hails from Asia, where he has been prized as a companion for more than a thousand years. He was a popular member of Chinese and Japanese imperial courts, and it was in Japan that his distinctive look was developed. This breed is elegant and dainty, mild-mannered and playful.'),
-(1,'Pembroke Welsh Corgi', '2017-1-1', 'null', 'Lulu', 19, 1, 2, 'Originally bred to herd cattle, sheep, and horses, the Pembroke Welsh Corgi is an active and intelligent dog breed. Easy to train and eager to learn, Pembrokes are great with children and other pets, and you can find them in four different coat colors and markings.'),
-(1,'Poodle', '2017-1-1', 'null', 'Muc', 19, 1, 3, 'Elegant. Proud. Clever. Poodles are impressive dogs, as the many best-in-show winners from this dog breed can attest. Behind the blue ribbons, impressive hairdos, and regal attitude, you’ll find an affectionate family dog with an ancient history and many talents.'),
-(1,'Pomeranian', '2017-1-1', 'null', 'Nini', 19, 1, 3, 'Descended from large sled dog breeds, the now-tiny Pomeranian has a long and interesting history. The foxy-faced dog, nicknamed “the little dog who thinks he can,” is compact, active, and capable of competing in agility and obedience or simply being a family friend.')
-
+GO
 Insert into dbo.datingMasterEntity  ( creDate, locat, [status], userID ) values
 ('2019-3-11', 'Ho Chinh Minh city', 1, 1),
 ('2018-4-17', 'Vung Tau beach', 1, 2),
 ('2018-5-10', 'Nha Trang city', 1, 3),
 ('2018-5-15', 'Ho Chi Minh city', 1, 1)
 
+GO
 Insert into dbo.datingDetailEntity  ( [datingDate], [datingLocation], [datingMaster_ID], [petRequest_ID], [petRecieve_ID], [isAccepted], [specialStatus], [isNewNotification])  values
 ('2019-1-1', '195 Cao Thang - District 3', 1, 1, 2, 1, 0, 1),
 ('2019-1-10', 'Tao Dan Park', 1, 1, 2, 1, 1, 1),
@@ -83,12 +87,14 @@ Insert into dbo.datingDetailEntity  ( [datingDate], [datingLocation], [datingMas
 ('2019-3-21', '23-9 park', 3, 2, 3, 1, 0, 1),
 ('2019-4-13', 'Pet Shop', 1, 2, 1, 1, 0, 1)
 
+GO
 Insert into dbo.tbCategory ([CateName], [Status]) values
 ('Healthy Food', 1),
 ('Pet Outfit', 1),
 ('Accessories',1),
 ('Pet Toy', 1)
 
+GO
 Insert into dbo.tbProduct ([ProImage], [Status], [Cate_ID] , [ProPrice], [ProColor], [ProName] , [ProDes]) values
 ('image', 1, 3, 70, 'black', 'dog hair cutting machine' , 'Type:
 Pet Cleaning & Grooming Products
@@ -121,6 +127,7 @@ INTERACTIVE DOG TOYS: Circular design of the Indoor Ball allows toy to roll acro
 ('image', 1, 2, 16, 'red', 'Indiana Hoosiers NCAA T-Shirt' , 'This Pets First Indiana Hoosiers T-Shirt is an official NCAA licensed pet shirt with your favorite team is name and logo. These team t-shirts are a must-have for any true NCAA fan who loves their dog as much as they love their team. You and your furry pal can celebrate your favorite team together, strengthening the bond between pet and Pet Parent with decorated team graphics you will both love!'),
 ('image', 1, 2, 45, 'black', 'Top Paw® Summer Fun "Beach Addict" Pet Tee' , 'Let your dog share in your love for the beach with this Top Paw Grey Beach Addict Tee. This fun tee features a beautiful beach scene as well as an admission of beach time addiction, and slips on and off easily while offering a comfortable fit. Only at PetSmart.')
 
+GO
 Insert into dbo.tbOrderMaster ([CreDate], [ShipDate], [Status], [User_ID]) values
 ('2019-10-10', '2019-11-12', 1, 1),
 ('2019-11-12', '2019-10-11', 1, 2),
@@ -128,6 +135,7 @@ Insert into dbo.tbOrderMaster ([CreDate], [ShipDate], [Status], [User_ID]) value
 ('2019-10-11', '2019-10-10', 1, 1),
 ('2019-12-10', '2019-11-11', 1, 2)
 
+GO
 Insert into dbo.tbOrderDetail ([OrderDate], [Qty], [Status], [OrderID], [Pro_ID]) values
 ('2019-1-2', 2, 1, 1, 1),
 ('2019-2-1', 3, 1, 2, 2),
